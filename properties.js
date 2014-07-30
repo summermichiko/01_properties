47a6eb4f7931b8cc94db40fd609c6cbcccf93bde
Summer <summermichiko@gmail.com>
2014-07-30 14:01:14 -0400

    added final solutions

--- properties.js
+++ properties.js
@@ -1,6 +1,6 @@
-var setSomePropertiesOn = function(obj_reference) {
-    obj_reference.x = 7; //reference dot something **(the dot dereferences the reference of obj, and then calls x on it)
-    obj_reference.y = 8; //can also say obj_reference["y"] = 8;
+var setSomePropertiesOn = function(obj) {
+    obj.x = 7; //reference dot something **(the dot dereferences the reference of obj, and then calls x on it)
+    obj.y = 8; //can also say obj_reference["y"] = 8;
     //using brackets allows you to insert a number saved as a variable or a string with a space (ex. "fullstack academy")
     
     //[] - accessing objects and arrays
@@ -9,12 +9,12 @@ var setSomePropertiesOn = function(obj_reference) {
     //obj_reference["fullstack academy"] = 8;
     //obj_reference[true] =8;
 
-    obj_reference.onePlus = function(number) {
+    obj.onePlus = function(number) {
         return number + 1;
     }
 
-    //could also be obj_reference["onePlus"] = function(number) {
-    	//...
+    //could also be obj["onePlus"] = function(number) {
+    	//return number + 1;
     }
 };
 
